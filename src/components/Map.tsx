341ec124466172c5b786492e88272190df83d64a
janatdeepsingh <janatdeep09singh@gmail.com>
2025-11-29 12:57:22 +0530

    fixes

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -9,6 +9,7 @@ import iconUrl from "leaflet/dist/images/marker-icon.png";
 import shadowUrl from "leaflet/dist/images/marker-shadow.png";
 // leaflet-default-icon-issue-solution
 
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 
 L.Icon.Default.mergeOptions({
   iconRetinaUrl,
